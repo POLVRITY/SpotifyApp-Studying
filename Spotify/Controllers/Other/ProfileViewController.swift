aa1b3c9e4d5dc416947044f29ad1e4de6409e049
Vladislav Beliy <vlad.belyy.2002@gmail.com>
2022-11-20 11:57:11 +0300

    [SpotifyApp-001] - Add a avatar in profile

--- Spotify/Controllers/Other/ProfileViewController.swift
+++ Spotify/Controllers/Other/ProfileViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Admin on 14.11.2022.
 //
 
+import SDWebImage
 import UIKit
 
 class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -57,7 +58,25 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         models.append("User ID: \(model.id)")
         models.append("Plan: \(model.product)")
 
+        createTableHeader(with: model.images.first?.url)
         tableView.reloadData()
+    }
+    
+    private func createTableHeader(with string: String?) {
+        guard let urlString = string, let url = URL(string: urlString) else { return }
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: view.width / 1.5))
+        
+        let imageSize: CGFloat = headerView.height / 2
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: imageSize, height: imageSize))
+        headerView.addSubview(imageView)
+        imageView.center = headerView.center
+        imageView.contentMode = .scaleAspectFill
+        imageView.sd_setImage(with: url, completed: nil)
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageSize / 2
+        
+        tableView.tableHeaderView = headerView
     }
     
     private func failedToGetProfile() {
