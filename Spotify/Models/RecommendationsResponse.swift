e221cbdfe534722b6bcc941786d612d688f0fa3e
Vladislav Beliy <vlad.belyy.2002@gmail.com>
2022-12-05 01:40:35 +0300

    [SpotifyApp-003] - Browse an API`s for recommended playlists and genres

---
+++ Spotify/Models/RecommendationsResponse.swift
@@ -0,0 +1,13 @@
+//
+//  RecommendationsResponse.swift
+//  Spotify
+//
+//  Created by POLVRITY on 05.12.2022.
+//
+
+import Foundation
+
+struct RecommendationsResponse: Codable {
+    let tracks: [AudioTrack]
+}
+
